a62e8d274cca96d9db580844d1db8652563d391c
David <89549981+Valenzcia@users.noreply.github.com>
2022-03-11 00:16:48 -0600

    Add read methods to doctor model

--- src/models/Doctor.java
+++ src/models/Doctor.java
@@ -10,34 +10,73 @@ import java.util.Scanner;
 public class Doctor {
     private static List<Doctor> all = new ArrayList<Doctor>();;
 
-    private int id;
-    private String firstName;
-    private String surname;
-    private String speciality;
+    public int id;
+    private boolean persisted;
+    public String firstName;
+    public String surname;
+    public String speciality;
+
+    /*
+    static {
+        try {
+            reload();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    */
+
+    public Doctor() {}
 
     public Doctor(String firstName, String surname, String speciality) {
-        this.id = 1;
+        this.persisted = false;
         this.firstName = firstName;
         this.surname = surname;
         this.speciality = speciality;
     }
 
+    public Doctor(int id, String firstName, String surname, String speciality) {
+        this.id = id;
+        this.firstName = firstName;
+        this.surname = surname;
+        this.speciality = speciality;
+        this.persisted = true;
+    }
+
     public String fullName() {
         return firstName + " " + surname;
     }
-    
-    public static void all() throws Exception {
+
+    public static Doctor find(int id) throws Exception {
+        //id.toString()
+        for (int i = 0; i < all().size(); i++) {
+            Doctor instance = all().get(i);
+            if (instance.id == id) {
+                return instance;
+            }
+        }
+        return new Doctor();
+    }
+
+    private static void reload() throws Exception {
         String line = "";
         String delimiter = ",";
         File file = new File("src/db/doctors.csv");
         BufferedReader br = new BufferedReader(new FileReader(file.getAbsolutePath()));
+        all.clear();
         while ((line = br.readLine()) != null) {
             String[] row = line.split(delimiter);
-            Doctor d = new Doctor(row[1], row[2], row[3]);
+            Doctor d = new Doctor(Integer.parseInt(row[0]), row[1], row[2], row[3]);
             all.add(d);
         }
-    } 
+    }
 
-    public void saveToDb() throws Exception {
+    public static List<Doctor> all() throws Exception {
+        reload();
+        return all;
+    }
+
+    public void save() throws Exception {
+
     }
 }
--- src/valencia/corporation/Main.java
+++ src/valencia/corporation/Main.java
@@ -4,7 +4,8 @@ import models.Doctor;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-	    Doctor d = new Doctor("Daniel", "Ávila", "Ginecólogo");
-        d.all();
+        Doctor d = Doctor.find(2);
+        System.out.println(d.fullName());
+        System.out.println(d.speciality);
     }
 }
