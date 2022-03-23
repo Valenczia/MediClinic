2d8f5d1f0b75680fa9388e75e0b06ebdad095af5
David <89549981+Valenzcia@users.noreply.github.com>
2022-03-22 19:40:48 -0600

    Add corporations

--- src/valencia/corporation/Main.java
+++ src/valencia/corporation/Main.java
@@ -4,9 +4,9 @@ import models.Doctor;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Doctor d = Doctor.find(2);
-        System.out.println(d.fullName());
-        System.out.println(d.speciality);
+        // Doctor d = Doctor.find(2);
+        // System.out.println(d.fullName());
+        // System.out.println(d.speciality);
         Doctor x = new Doctor("Carlos", "Mireles", "Pediatra");
         x.save();
         System.out.println("Cuantos doctores hay: " + Doctor.all().size());
